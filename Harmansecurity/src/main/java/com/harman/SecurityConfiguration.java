6f07bf21ccd48d418355d1672d19a89747ce7916
abdul <abdul.tanveer@gmail.com>
2022-05-09 11:55:07 +0530

    authentication manager

---
+++ Harmansecurity/src/main/java/com/harman/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.harman;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			// TODO Auto-generated method stub
+			super.configure(auth);
+			auth.inMemoryAuthentication()
+			.withUser("aditya")
+			.password("harman")
+			.roles("USER");
+		}
+		
+		@Bean  //@Bean means hey spring  create and inject this bean[PasswordEncoder]
+		public PasswordEncoder getPasswordEncoder() {
+			return NoOpPasswordEncoder.getInstance();
+		}
+		
+}
