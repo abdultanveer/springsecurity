b34cb982a6e8da3e4847f1bb73433a50973ffad7
abdul <abdul.tanveer@gmail.com>
2022-05-09 12:27:25 +0530

    authorization

--- Harmansecurity/src/main/java/com/harman/SecurityConfiguration.java
+++ Harmansecurity/src/main/java/com/harman/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.harman;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -27,6 +28,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		@Bean  //@Bean means hey spring  create and inject this bean[PasswordEncoder]
 		public PasswordEncoder getPasswordEncoder() {
 			return NoOpPasswordEncoder.getInstance();
+		}
+		
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+			.antMatchers("/**")
+			.hasRole("ADMIN")
+			.and()
+			.formLogin();
+			
+		
 		}
 		
 }
