cbef61624fa29851e78bcbbe0096b19703dcaa1b
abdul <abdul.tanveer@gmail.com>
2022-05-09 10:49:16 +0530

    initial commit

---
+++ Harmansecurity/src/main/java/com/harman/HarmansecurityApplication.java
@@ -0,0 +1,13 @@
+package com.harman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmansecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmansecurityApplication.class, args);
+	}
+
+}
